0f7d74782c0cdc67585dc079f57eeea9b418b77e
Pnonato <like.nonato.marrara@gmail.com>
2026-09-21 20:10:58 -0300

    feat: adicona logica para verificar se horario esta livre na agenda do barbeiro

--- src/main/java/br/ifsp/demo/domain/agendamento/AgendaDoBarbeiro.java
+++ src/main/java/br/ifsp/demo/domain/agendamento/AgendaDoBarbeiro.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.domain.agendamento;
 
 import br.ifsp.demo.domain.comum.BarbeiroId;
+import br.ifsp.demo.domain.comum.Periodo;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,5 +15,16 @@ public class AgendaDoBarbeiro {
         this.barbeiroId = barbeiroId;
         this.data = data;
         this.agendamentosDoDia = List.copyOf(agendamentosDoDia);
+    }
+    public boolean estaLivre(Periodo periodo, AgendamentoId agendamentoAtual) {
+        for (Agendamento agendamento : agendamentosDoDia) {
+            if (agendamento.getId().equals(agendamentoAtual)) {
+                continue;
+            }
+            if (agendamento.getPeriodo().sobrepoe(periodo)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
