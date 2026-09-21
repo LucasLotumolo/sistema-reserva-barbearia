616ec4ad988ee6b158b8173b9343b5179e934f05
Pnonato <like.nonato.marrara@gmail.com>
2026-09-21 20:08:15 -0300

    feat: adicona logica para verificar sobreposicao de periodos

--- src/main/java/br/ifsp/demo/domain/comum/Periodo.java
+++ src/main/java/br/ifsp/demo/domain/comum/Periodo.java
@@ -3,4 +3,7 @@ package br.ifsp.demo.domain.comum;
 import java.time.LocalDateTime;
 
 public record Periodo(LocalDateTime inicio, LocalDateTime fim) {
+    public boolean sobrepoe(Periodo outro) {
+        return this.inicio().isBefore(outro.fim()) && outro.inicio().isBefore(this.fim());
+    }
 }
